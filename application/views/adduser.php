eea378ca9c2d18ffdc2aa70b13e40e242fcb1ca0
niju <niju.lv@rainconcert.in>
2016-05-02 11:05:38 +0530

    design issue solved

--- application/views/adduser.php
+++ application/views/adduser.php
@@ -25,72 +25,55 @@
                 <?php } ?>
                </div>
             </div>
-	    <form class="form-label-left form-horizontal" enctype="multipart/form-data" method ="POST" action="<?php echo base_url() ?>adduser" name="frmUserdetails" id="frmUserdetails">   
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtFirstname">First Name<span class="required">*</span>
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <input type="text" class="form-control col-md-7 col-xs-12" name="firstname" id="firstname" value="<?php echo set_value('firstname') ?>" required>
+	    <form class="" enctype="multipart/form-data" method ="POST" action="<?php echo base_url() ?>adduser" name="frmUserdetails" id="frmUserdetails">  
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="txtFirstname">First Name<span class="required">*</span></label>
+                                        <input type="text" class="form-control" name="firstname" id="firstname" value="<?php echo set_value('firstname') ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="txtLastname">Last Name</label>
+                                        <input type="text" class="form-control" name="lastname" id="lastname" value="<?php echo set_value('lastname') ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="txtUseremail">Email<span class="required">*</span></label>
+                                        <input type="email" class="form-control" name="useremail" id="useremail" value="<?php echo set_value('useremail') ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label  for="txtPhonenumber">Phone<span class="required">*</span></label>
+                                        <input type="text" class="form-control" name="phonenumber" id="phonenumber" value="<?php echo set_value('phonenumber') ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="txtUseraddress">Address</label>
+                                        <textarea class="form-control" name="useraddress" id="useraddress" style="height:150px ! important;" value="<?php echo set_value('address') ?>" ></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label  for="txtPincode">Pincode</label>
+                                        <input type="text" class="form-control" name="pincode" id="pincode" value="<?php echo set_value('pincode') ?>" >
+                                     </div>
+                                    <div class="form-group">
+                                        <label  for="txtAttachment">Attachment</label>
+                                        <input type="file" class="form-control" name="attachment" id="attachment" style="height:500px;padding-bottom: 48px;" value="<?php echo set_value('attachment') ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="txtUserstatus">Status<span class="required">*</span></label>
+                                        <select name="userstatus" id="Userstatus" class="form-control">
+                                            <option value="0">Guest</option>
+                                            <option value="1">User</option>
+                                        </select>
+                                    </div> 
+                                    <div class="form-group">
+                                        <button type="reset" id="btnCancel" class="btn btn-default">Cancel</button>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtLastname">Last Name
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <input type="text" class="form-control col-md-7 col-xs-12" name="lastname" id="lastname" value="<?php echo set_value('lastname') ?>">
-                        </div>
-                    </div>
-                   <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtUseremail">Email<span class="required">*</span>
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <input type="email" class="form-control col-md-7 col-xs-12" name="useremail" id="useremail" value="<?php echo set_value('useremail') ?>" required>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtPhonenumber">Phone<span class="required">*</span>
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <input type="text" class="form-control col-md-7 col-xs-12" name="phonenumber" id="phonenumber" value="<?php echo set_value('phonenumber') ?>" required>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="txtUseraddress">Address
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <textarea class="form-control col-md-7 col-xs-12" name="useraddress" id="useraddress" style="height:150px ! important;" value="<?php echo set_value('address') ?>" ></textarea>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtPincode">Pincode
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <input type="text" class="form-control col-md-7 col-xs-12" name="pincode" id="pincode" value="<?php echo set_value('pincode') ?>" >
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtAttachment">Attachment
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <input type="file" class="form-control col-md-7 col-xs-12" name="attachment" id="attachment" style="height:500px;padding-bottom: 48px;" value="<?php echo set_value('attachment') ?>">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 " for="txtUserstatus">Status<span class="required">*</span>
-                        </label>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <select name="userstatus" id="Userstatus" class="form-control col-md-7 col-xs-12">
-                                <option value="0">Guest</option>
-                                <option value="1">User</option>
-                            </select>
-                        </div>
-                    </div>
-                <div class="ln_solid"></div>
-                    <div class="form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                           <button type="reset" id="btnCancel" class="btn btn-primary">Cancel</button>
-                            <button type="submit" class="btn btn-success">Submit</button>
-                        </div>
-                    </div>
-                </form>
+                </div>
+            </form>
 </div>
--- application/views/viewuser.php
+++ application/views/viewuser.php
@@ -20,80 +20,87 @@
                 if($user_details['status']==2){
                 $status="Deleted";
                 }?>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>First Name : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['first_name']; ?>
-                </div>
-            </div>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Last Name : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['last_name']; ?>
-                </div>
-            </div>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Email : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['email']; ?>
-                </div>
-            </div>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Phone : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['phone']; ?>
-                </div>
-            </div>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Address : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['address']; ?>
-                </div>
-            </div>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Pincode : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['pincode']; ?>
-                </div>
-            </div>
-            <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Attachment : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $user_details['attachments']; ?>
-                </div>
-            </div>
-             <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b>Status : </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <?php echo $status; ?>
-                </div>
-            </div>
-             <div class="row" style="padding-bottom: 15px;">
-                <div class="col-lg-3 col-sm-3 col-md-3">
-                    <b> </b>
-                </div>
-                <div class="col-lg-8 col-sm-8 col-md-8">
-                    <form action="<?php echo base_url() ?>manageuser">
-                     <button type="submit" class="btn btn-success">Back to list</button>
-                    </form>
-                </div>
-            </div>
-            
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>First Name : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['first_name']; ?>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Last Name : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['last_name']; ?>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Email : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['email']; ?>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Phone : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['phone']; ?>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Address : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['address']; ?>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Pincode : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['pincode']; ?>
+                                    </div>
+                                </div>
+                                <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Attachment : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $user_details['attachments']; ?>
+                                    </div>
+                                </div>
+                                 <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b>Status : </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <?php echo $status; ?>
+                                    </div>
+                                </div>
+                                 <div class="row" style="padding-bottom: 15px;">
+                                    <div class="col-lg-3 col-sm-3 col-md-3">
+                                        <b> </b>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-md-8">
+                                        <form action="<?php echo base_url() ?>manageuser">
+                                         <button type="submit" class="btn btn-default">Back to list</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
             <?php } ?>
 </div>
